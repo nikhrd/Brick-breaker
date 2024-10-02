1f499bfc4d7ecd7b286a74b16403f1f1be52d107
nikhrd <23c08.nikhithar@sjec.ac.in>
2024-10-02 17:48:36 +0530

    first commit

--- src/brickBreaker/brickB.java
+++ src/brickBreaker/brickB.java
@@ -3,10 +3,11 @@ import javax.swing.*;
 public class brickB {
 	public static void main(String[] args) {
 		JFrame jf=new JFrame("Brick Breaker");
-		new gamePlay();
+		gamePlay gm=new gamePlay();
 		jf.setBounds(10,10,700,600);
-		jf.setLayout(null);
+		jf.setResizable(false);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.add(gm);
 		jf.setVisible(true);
 	}
 }
--- src/brickBreaker/gamePlay.java
+++ src/brickBreaker/gamePlay.java
@@ -1,5 +1,116 @@
 package brickBreaker;
 import javax.swing.*;
-public class gamePlay extends JPanel {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.*;
+import javax.swing.Timer;
+
+public class gamePlay extends JPanel implements KeyListener,ActionListener{
+	private boolean play=false;
+	private int score=0;
+	private int totalBricks=25;
+	private Timer time;
+	private int delay=8;
+	private int playerX=310;
+	private int ballposx=120;
+	private int ballposy=350;
+	private int ballXdir=-1;
+	private int ballYdir=-2;
 	
+	private MapGenerator map;
+	
+	public gamePlay() {
+		map = new MapGenerator(3,7);
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		requestFocusInWindow();
+		time=new Timer(delay,this);
+		time.start();
+	}
+	
+	public void paint(Graphics g) {
+		//for background
+		g.setColor(Color.black);
+		g.fillRect(1,1,692,592);
+		
+		//drawing map
+		map.draw((Graphics2D)g);
+		
+		//borders
+		/*g.setColor(Color.red);
+		g.fillRect(0, 0, 3,592);
+		g.fillRect(0, 0, 692,3);
+		g.fillRect(691, 0, 3,592);*/
+		
+		//the paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		//ball
+		g.setColor(Color.cyan);
+		g.fillOval(ballposx, ballposy, 20, 20);
+		
+		g.dispose();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		time.start();
+		if(play) {
+			if(new Rectangle(ballposx,ballposy,20,20).intersects(new Rectangle(playerX,550,100,8))) {
+				ballYdir = -ballYdir;
+			}
+			ballposx += ballXdir;
+			ballposy += ballYdir;
+			
+			if(ballposx<0) {
+				ballXdir = -ballXdir;
+			}
+			if(ballposy<0) {
+				ballYdir = -ballYdir;
+			}
+			if(ballposx>670) {
+				ballXdir = -ballXdir;
+			}
+		}
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if(playerX >=600) {
+				playerX=600;
+			}
+			else {
+				moveRight();
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			if(playerX <10) {
+				playerX=10;
+			}
+			else {
+				moveLeft();
+			}
+		}	
+	}
+	public void moveRight() {
+		play=true;
+		playerX +=20;
+		repaint();
+	}
+	public void moveLeft() {
+		play=true;
+		playerX -=20;
+		repaint();
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {}
 }
